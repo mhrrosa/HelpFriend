8b3933dc5ed366d9073f9a7f747d95612c349db2
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2022-05-02 09:10:04 -0300

    exclue o id do cachorro e funcionário

--- cadastroCachorro.php
+++ cadastroCachorro.php
@@ -13,7 +13,6 @@
             <div class="header-1">
                 <div class="logo">
                     <img src="IMG/logo.jpeg" height="120" />
-
                 </div>
                 <div class="botao-inicio-login">
                     <ul>
@@ -27,14 +26,9 @@
                                 <h3>SOBRE</h3>
                             </a>
                         </li>
-
-
                     </ul>
-
                 </div>
-
             </div>
-
         </main>
     </header>
     <header class="linha-1"></header>
@@ -43,52 +37,44 @@
         <div class="formcadastro">
             <form id="cadastro" action="cadastroCachorro_exe.php" method="post" onsubmit="return check(this.form)" enctype="multipart/form-data">>
                 <div class="form">
-                    <label for="text"> Cadastro de Cachorro
-                        </label>
-
-                    <label for="name"> id 
-                            <input type="text" name="id">
-                    </label>
+                    <label for="text"> Cadastro de Cachorro</label>
                     <label for="name"> Nome 
-                            <input type="text" name="nome">
-                        </label>
+                        <input type="text" name="nome">
+                    </label>
                     <label for="date"> Ano 
-                            <input type="date" name="ano">
-                        </label>
-
+                        <input type="date" name="ano">
+                    </label>
                     <label for="name"> Porte 
-                            <select name="porte" name="porte">
-                                <option value="Selecione o porte desejado:"></option>
-                                <option value="Pequeno">Pequeno</option>
-                                <option value="Médio">Medio</option>
-                                <option value="Grande">Grande</option>
-                            </select>
-                        </label>
-
+                        <select name="porte" name="porte">
+                            <option value="Selecione o porte desejado:"></option>
+                            <option value="Pequeno">Pequeno</option>
+                            <option value="Médio">Medio</option>
+                            <option value="Grande">Grande</option>
+                        </select>
+                    </label>
                     <label for="name"> Raca
-                                <select name="raca">
-                                    <option value="Selecione a raca desejada:"></option>
-                                    <option value="Shih Tzu">Shih Tzu</option>
-                                    <option value="Yorkshire">Yorkshire</option>
-                                    <option value="Buldogue frances">Buldogue frances</option>
-                                    <option value="Maltes">Maltes</option>
-                                    <option value="Golden Retriever">Golden Retriever</option>
-                                    <option value="Labrador Retriever">Labrador Retriever</option>
-                                    <option value="Pug">Pug</option>
-                                    <option value="Pinscher">Pinscher</option>
-                                    <option value="Schnauzer">Schnauzer</option>
-                                    <option value="Beagle">Beagle</option>
-                                    <option value="Border Collie">Border Collie</option>
-                                    <option value="Raca nao definida">Raca nao definida</option>      
-                                </select>
-                        </label>
+                        <select name="raca">
+                            <option value="Selecione a raca desejada:"></option>
+                            <option value="Shih Tzu">Shih Tzu</option>
+                            <option value="Yorkshire">Yorkshire</option>
+                            <option value="Buldogue frances">Buldogue frances</option>
+                            <option value="Maltes">Maltes</option>
+                            <option value="Golden Retriever">Golden Retriever</option>
+                            <option value="Labrador Retriever">Labrador Retriever</option>
+                            <option value="Pug">Pug</option>
+                              <option value="Pinscher">Pinscher</option>
+                            <option value="Schnauzer">Schnauzer</option>
+                            <option value="Beagle">Beagle</option>
+                            <option value="Border Collie">Border Collie</option>                                
+                            <option value="Raca nao definida">Raca nao definida</option>      
+                        </select>
+                    </label>
                     <label for="name"> id_instituicao 
                             <input type="text" name="id_instituicao">
                     </label>
                     <label for="submit"> 
                             <button type="submit"><b>Cadastrar</b></button>
-                        </label>
-
+                    </label>
                 </div>
             </form>
         </div>
--- cadastroFuncionario.php
+++ cadastroFuncionario.php
@@ -12,20 +12,14 @@
                 <div class="header-1">
                     <div class="logo">
                         <img src="IMG/logo.jpeg" height="120"/>
-        
                     </div>
                     <div class="botao-inicio-login">
                         <ul>
                             <li><a href=""><h3>MENU</h3></a></li>
                             <li><a href=""><h3>SOBRE</h3></a></li>
-                            
-
                         </ul>
-        
-        
                     </div>
                 </div>
-        
             </main>
         </header>
         <header class="linha-1"></header>
@@ -34,13 +28,7 @@
             <div class="formcadastro">
                 <form id="cadastro" action="cadastroFuncionario_exe.php" method="post" onsubmit="return check(this.form)" enctype="multipart/form-data">>
                     <div class="form">
-                        <label for="text"> Cadastro de Funcionário
-                        </label>
-                        <label for="id"> id Funcionario 
-                            <input type="text" name="idf">
-                        </label>
-
-                        
+                        <label for="text"> Cadastro de Funcionário</label>
                         <label for="name"> Nome 
                             <input type="text" name="nome">
                         </label>
