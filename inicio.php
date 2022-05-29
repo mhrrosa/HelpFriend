a05d95c7c71f808be19416d243236a72f0e0a6ab
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2022-05-29 01:14:15 -0300

    Atualização de centralização

--- inicio.php
+++ inicio.php
@@ -81,7 +81,7 @@
     <div class="w3-main w3-container">
 
         <div class="w3-panel w3-padding-large w3-card-4 w3-light-dark">
-            <h1 class="w3-xxlarge"style="margin-left:43%">Cachorros</h1>
+            <h1 class="w3-xxlarge"style="margin-left:46%">Cachorros</h1>
 
             <p class="w3-large">
             <div class="w3-code">  
