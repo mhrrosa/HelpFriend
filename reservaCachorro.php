bc2e58c7df3b332ffe9918dfee4fce3ca97e7f85
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2022-06-07 20:15:57 -0300

    tela reservar cachorro

---
+++ reservaCachorro.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">    
+        <link rel="stylesheet" type="text/css" href="CSS/reservar.css">
+        <title>Reserva de Cachorro - Help Friend</title>
+        <link rel="icon" type="image/jpg" href="IMG/logo_icone.jpg"/>
+    </head>
+    <body>
+    <!-- CONEXÃO COM O BANCO DE DADOS -->
+    <?php require 'conectaBD.php'; ?>
+        <!-- CABEÇALHO -->
+        <header class="cabecalho">
+            <div>
+                <img class="logo" src="IMG/logopng.png"/>
+            </div>
+            <div class="botao-cabecalho">
+                <ul>
+                    <li>
+                        <a href="">
+                            <h3>MENU</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <h3>SOBRE</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <h3>CONTATO</h3>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+        <!-- LINHA DE DIVISÃO -->
+        <header class="linha-divisao"></header>
+        <div>
+            <div>
+                <!-- ACESSO AO BANCO DE DADOS-->
+                <?php		
+                    $id=$_GET['id'];
+
+                    // Cria conexão
+                    $conn = mysqli_connect($servername, $username, $password, $database);
+
+                    // Verifica conexão
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                    }
+                    // Configura para trabalhar com caracteres acentuados do português	 
+                    mysqli_query($conn,"SET NAMES 'utf8'");
+                    mysqli_query($conn,'SET character_set_connection=utf8');
+                    mysqli_query($conn,'SET character_set_client=utf8');
+                    mysqli_query($conn,'SET character_set_results=utf8');
+
+                    // Faz Select na Base de Dados
+
+                    $sql = "SELECT Id, email, Senha from adotante where Id = $id";
+
+                    if ($result = mysqli_query($conn, $sql)) {
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $id = $row['Id'];
+                                $nome = $row['nome'];
+                                $senha = $row['Senha'];
+                        }
+                    }
+                ?>
+                <!-- FORMULÁRIO -->
+                <div class="formconfirma">
+                    <form id="cadastro" action="reservaCachorro_exe.php" method="post" onsubmit="return check(this.form)" enctype="multipart/form-data">
+                    <input type="hidden" id="Id" name="Id" value="<?php echo $id; ?>">  
+                    <div class="form">
+                            <label for="text" class="titulo-form" ><b>CONFIRMAÇÃO DE CADASTRO</b></label>
+                            <label for="name"> Nome 
+                                <input type="text" name="nome" value="<?php echo $nome; ?>">
+                            </label>
+                            <label for="password"> Senha 
+                                <input type="password" name="senha" value="<?php echo $senha; ?>">
+                            </label>
+                            <label for="submit"> 
+                                <button type="submit" style="max-width: 100px"><b>Confirmar</b></button>
+                            </label>
+                        </div>
+                    </form>
+                <?php     
+                        
+                    }else {
+                        echo "Erro executando UPDATE: " . mysqli_error($conn);
+                    }
+                    //FIM DA DIV FORM
+                    echo "</div>";
+                    //ENCERRA CONEXÃO COM O BANCO DE DADOS
+                    mysqli_close($conn); //Encerra conexao com o BD
+
+                ?>
+
+            </div>
+        </div>
+        <!-- RODAPÉ -->
+        <footer>
+            <header class="linha-divisao"></header>
+            <img class="img-rodape" src="IMG/logo_verticalpng.png">
+            <p class="copyright" style="text-align: center;">&copy; Copyright Help Friend - 2022</p>
+        </footer>
+    </body>
+</html>
---
+++ reservaCachorro_exe.php
@@ -0,0 +1,81 @@
+<html>
+	<head>
+	  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	  <link rel="stylesheet" type="text/css" href="CSS/reservar.css"/>
+      <link rel="icon" type="image/jpg" href="IMG/logo_icone.jpg"/>
+      <title>Reserva de Cachorro - Help Friend</title>
+	</head>
+    <header class="cabecalho">
+            <div>
+				<img class="logo" src="IMG/logopng.png"/>
+            </div>
+            <div class="botao-cabecalho">
+                <ul>
+                    <li><a href="">
+                            <h3>MENU</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <h3>SOBRE</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <h3>CONTATO</h3>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    </header>
+    <header class="linha-divisao"></header>
+    <body>
+        <div class="w3-main w3-container" style="margin-left:10px;margin-top:117px;">
+
+            <div class="w3-panel w3-padding-large w3-card-4 w3-light-grey">
+                <h1 class="w3-xxlarge">Reserva de Cachorro</h1>
+
+                    <p class="w3-large">
+                        <div class="w3-code cssHigh notranslate">
+                            <?php require 'conectaBD.php'; ?>
+                            <?php
+                                $id =  $_POST['Id'];
+                                $nome = $_POST['nome'];
+                                $senha = $_POST['Senha'];
+
+                                $conn = mysqli_connect($servername, $username, $password, $database);
+
+                                // Verifica conexão 
+                                if (!$conn) {
+                                    die("Falha na conexão com o Banco de Dados: " . mysqli_connect_error());
+                                }
+
+                                // Configura para trabalhar com caracteres acentuados do português
+                                mysqli_query($conn, "SET NAMES 'utf8'");
+                                mysqli_query($conn, 'SET character_set_connection=utf8');
+                                mysqli_query($conn, 'SET character_set_client=utf8');
+                                mysqli_query($conn, 'SET character_set_results=utf8');
+
+                                $sql = "SELECT FROM adotante WHERE nome == $nome AND Senha == $senha";
+                                // Faz o Upadate na Base de Dados
+
+                                if ($result = mysqli_query($conn, $sql)) {
+                                    echo "Cachorro Reservado";
+                                } else {
+                                    
+                                    echo "Erro executando a atualização: " . mysqli_error($conn);
+                                }
+                                echo "</div>";
+                                mysqli_close($conn);  //Encerra conexao com o BD
+                            ?>
+                        </div>
+                    </p>
+            </div>
+        </div>
+        <footer>
+            <header class="linha-divisao"></header>
+            <img class="img-rodape" src="IMG/logo_verticalpng.png">
+            <p class="copyright">&copy; Copyright Help Friend - 2022</p>
+        </footer>
+    </body>
+</html>
